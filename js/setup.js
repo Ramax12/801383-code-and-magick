2795e274375fd048bb448febb06ae3f4c348f867
Maksim Ko <Ramax13@gmail.com>
2018-06-15 04:15:05 +0300

    Добавил исправления

--- js/setup.js
+++ js/setup.js
@@ -102,15 +102,24 @@ similarListElement.appendChild(fragment);
 var changeCoat = document.querySelector('.setup-wizard .wizard-coat');
 var changeEyes = document.querySelector('.setup-wizard .wizard-eyes');
 var changeFireball = document.querySelector('.setup-fireball-wrap');
+var inputChangeCoat = document.querySelector('input[name=coat-color]');
+var inputChangeEyes = document.querySelector('input[name=eyes-color]');
+var inputChangeFireball = document.querySelector('input[name=fireball-color]');
 
 changeCoat.addEventListener('click', function () {
-  changeCoat.style.fill = renderCoat();
+  var color = renderCoat();
+  changeCoat.style.fill = color;
+  inputChangeCoat.value = color;
 });
 
 changeEyes.addEventListener('click', function () {
-  changeEyes.style.fill = renderEyes();
+  var color = renderEyes();
+  changeEyes.style.fill = color;
+  inputChangeEyes.value = color;
 });
 
 changeFireball.addEventListener('click', function () {
-  changeFireball.style.background = renderFireball();
+  var color = renderFireball();
+  changeFireball.style.background = color;
+  inputChangeFireball.value = color;
 });
