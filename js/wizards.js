455773a24500bd6201d5c01395a4b4a5ce4ce7b1
Maksim Ko <Ramax13@gmail.com>
2018-07-01 16:26:22 +0300

    Исправление

--- js/wizards.js
+++ js/wizards.js
@@ -9,12 +9,10 @@
   window.FIREBALL_COLOR = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
   document.querySelector('.setup-similar').classList.remove('hidden');
-  var setup = document.querySelector('.setup');
   var similarListElement = document.querySelector('.setup-similar-list');
   var similarWizardTemplate = document.querySelector('#similar-wizard-template')
       .content
       .querySelector('.setup-similar-item');
-  var form = document.querySelector('.setup-wizard-form');
 
   window.renderName = function () {
     var randomNames = Math.floor(Math.random() * NAMES.length);
@@ -34,31 +32,11 @@
     return wizardElement;
   };
 
-  var render = function () {
+  window.wizards = function () {
     var fragment = document.createDocumentFragment();
     for (var i = 0; i < 4; i++) {
       fragment.appendChild(renderWizard());
     }
     similarListElement.appendChild(fragment);
-  };
-
-  var onSuccess = function () {
-    render();
-    setup.classList.add('hidden');
-  };
-
-  var onError = function (textError) {
-    var node = document.createElement('div');
-    node.style.position = 'absolute';
-    node.style = 'z-index: 9; background-color: red; font-size: 24px;';
-    node.textContent = textError;
-    document.body.insertAdjacentElement('afterbegin', node);
-
-    form.addEventListener('submit', function (evt) {
-      window.backend.save(new FormData(form), onSuccess, onError);
-      evt.preventDefault();
-    });
-
-    window.backend.load(onSuccess, onError);
   };
 })();
